196b32d955d96efba1a4b6210647ac3f03d5d50b
Rachel Inderhees <rachelinderhees@gmail.com>
2020-04-22 11:47:42 -0500

    React implementation; Vue boilerplate
    React implementation -- need a little finesse on the algorithm. Vue implementation -- boilerplate.

--- react/hexacolour/src/App.js
+++ react/hexacolour/src/App.js
@@ -1,34 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="app_wrapper">
-        <div className="header">
-          <div className="text">
-            
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      colour: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.setColour = this.setColour.bind(this);
+  }
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value }, this.setColour);
+  }
+
+  setColour = () => {
+    let colour = '';
+    const input = this.state.value.split('');
+    for (const val of input) {
+      const asc = val.charCodeAt(0);
+      const dec = asc.toString(16);
+      colour = colour.concat(dec)
+    }
+    document.body.style.background = "#" + colour;
+    console.log(colour);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="app_wrapper">
+          <div className="header">
+            Welcome to HexaColour
+          </div>
+          <div className="body">
+              Find your colour: &nbsp; 
+              <input className="input" value={this.state.value} onChange={this.handleChange} />
           </div>
         </div>
       </div>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ vue/hexacolour/src/components/colourConverter.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="app_wrapper">
+        <div class="header_wrapper">
+            {{ header }}
+        </div>
+        <div class="text_wrapper">
+            {{ text }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ColourConverter',
+    props: {
+        header: any,
+        text: any
+    }
+}
+</script>
